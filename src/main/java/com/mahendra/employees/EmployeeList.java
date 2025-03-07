dc305ca09a9a1c23021b1c40c8ded0df78884101
demouser4232 <demouser4232@outlook.com>
2025-03-07 17:04:12 +0000

    Update EmployeeList.java

--- src/main/java/com/mahendra/employees/EmployeeList.java
+++ src/main/java/com/mahendra/employees/EmployeeList.java
@@ -14,7 +14,7 @@ public class EmployeeList {
     static{
         employeeList.add(new Employee("Sivaa","Nethaji","12-12-1990","Lead","IT","sivaa.nethaji@abc.com"));
         employeeList.add(new Employee("Yaaro","Evaro","02-11-1979","Manager","IT","yaaro.evaro@etho.com"));
-        employeeList.add(new Employee("Vetti","Office","22-10-1966","VIP","VIP","vetti.officer@vip.com"));
+        employeeList.add(new Employee("Vetti","Officer","22-10-1966","VIP","VIP","vetti.officer@vip.com"));
         employeeList.add(new Employee("Joana","Sanders","11-11-1976","Manager","Marketing","joana.sanders@abc.com"));
         employeeList.add(new Employee("John","Drake","18-08-1988","Coordinator","Finance","john.drake@abc.com"));
         employeeList.add(new Employee("Samuel","Williams","22-03-1985","Coordinator","Finance","samuel.williams@abc.com"));
